bf040aac51ceba9a9baa8d6b97daefc63c9468c7
user <user@DESKTOP-IJJ8LDQ>
2023-12-28 14:22:26 +0100

    Add project files.

---
+++ utilities/utilities.h
@@ -0,0 +1,11 @@
+#ifndef utilities_h
+#define utilities_h
+
+#include <vector>
+#include <algorithm>
+#include "..//headers/player.h"
+
+
+std::vector<Player*> DetermineTurnOrder(std::vector<Player*> players);
+
+#endif
